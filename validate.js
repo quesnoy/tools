ecd160e32f96bb77688c1711d00ef25fcb3b7fc3
artring <info@artring.jp>
2015-06-11 20:47:19 +0900

    Replace message function

--- validate.js
+++ validate.js
@@ -26,6 +26,15 @@ function formValidate ( theForm ) {
 	this.isAlphaNumeric = function ( el, param ) {
 		return /^[A-Za-z0-9]+$/.test(this.form.elements.namedItem(el).value) ? true : this.defaultMessages['isAlphaNumeric'];
 	}
+	
+	//REPLACE DEFAULT MESSAGE IF PRESENT
+	this.setMessages = function ( messages ) {
+		for ( var key in messages ) {
+			if ( messages.hasOwnProperty(key) && this.defaultMessages.hasOwnProperty(key) ) {
+				this.defaultMessages[key] = messages[key];
+			}
+		}
+	}
 
 	//DISPLAY ERRROR MESSAGE ( ALERT )
 	this.displayErrors = function () {
@@ -43,18 +52,22 @@ function formValidate ( theForm ) {
 	//VALIDATE THE FORM
 	//LOOP TROUGHT THE RULES, AND VALIDATE EACH ONE, THEN RETURN ERROR MESSAGE IF FALSE
 	this.validate = function ( rules, messages ) {
+	
+		//SET MESSAGES
+		this.setMessages(messages);
+	
 		for (var key in rules) {
 			if (rules.hasOwnProperty(key)) {
 			        for ( var rule in rules[key] ) {
 					if (rules[key].hasOwnProperty(rule)) {
-			      		var check = this[rule](key, rules[key][rule]);
-			      		if (  check !== true ) {
-			      			if ( typeof this.errorMessages[key] === 'undefined' ) {
-			      				this.errorMessages[key] = [];
-			      			}
-			      			this.errorMessages[key].push(messages[key] + check);
-			      		}	
-			      	}
+			      			var check = this[rule](key, rules[key][rule]);
+			      			if (  check !== true ) {
+			      				if ( typeof this.errorMessages[key] === 'undefined' ) {
+			      					this.errorMessages[key] = [];
+			      				}
+			      				this.errorMessages[key].push(messages[key] + check);
+			      			}	
+			      		}
 			        }
 			}
 		}
