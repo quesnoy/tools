7b952a7beee1f5cc9d798c3ab6f27b56628040f0
quesnoy <quesnoy@users.noreply.github.com>
2015-07-27 15:13:37 +0900

    Method required, isEmail added

--- phpValidation/validation.php
+++ phpValidation/validation.php
@@ -1,12 +1,15 @@
 <?php
-    function isInt($value) {
-        return is_int($value);
+	function required ($value) {
+		return ( $value != '' ? true : false );
+	}
+    function isInt ($value) {
+        return ( preg_match('/^[0-9]*$/', $value) == 1 ? true : false );
     }
-    function isAlpha($value) {
-        if ( preg_match('/^[a-zA-Z]*$/', $value) == 1 ) {
-            return true;
-        } else {
-            return false;
-        }
+    function isAlpha ($value) {
+		return ( preg_match('/^[a-zA-Z]*$/', $value) == 1 ? true : false );
     }
+	function isEmail ($value) {
+		return ( preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i', $value) == 1 ? true : false );	
+	}
 ?>
+
