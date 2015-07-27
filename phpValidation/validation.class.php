af7427aca5cc4f9f2a4e6b687654a66af903a561
quesnoy <quesnoy@users.noreply.github.com>
2015-07-27 15:16:07 +0900

    Add errors messages display, default message

--- phpValidation/validation.class.php
+++ phpValidation/validation.class.php
@@ -2,18 +2,31 @@
 
     include "validation.php";
 
+	define('RULE_KEY', 0);
+	define('RULE_KEY_NAME', 1);
+	define('RULE_METHOD', 2);
+	define('RULE_ERROR_MESSAGE', 3);
+	
     class Validation {
 
         protected $values = array();
         protected $rules = array();
+		protected $errors = array();
+		
+		protected $defaultErrors = array(
+					'required' => '%s is needed!', 
+					'isAlpha' => '%s must be alphabet only!', 
+					'isInt' => '%s must be a integer only!', 
+					'isEmail' => '%s must be a valid email!', 
+		);
 
         //CONSTRUCTOR
         //$values : array
         // 'name' => 'tom'
         // 'age' => 24
         // 'email' => tom@yahoo.com
-        //$rules : array(array(key, validation(, error message)))
-        // 'age', 'isInt', '%s must be a number'
+        //$rules : array(array(key, key name, method(, error message)))
+        // 'age', 'isInt', 'the age', '%s must be a number'(printf format)
         public function __construct($values, $rules) {
 
             //VALUES
@@ -23,15 +36,15 @@
 
             //RULES
             foreach ( $rules as $k => $rule ) {
-                if ( ( empty($rule) ) || ( !isset($rule[0]) ) || ( !isset($rule[1]) ) ) {
+                if ( ( empty($rule) ) || ( !isset($rule[RULE_KEY]) )|| ( !isset($rule[RULE_KEY_NAME]) ) || ( !isset($rule[RULE_METHOD]) ) ) {
                     echo "ERROR : NO RULES SET";
                     exit;
                 }
 
-                if ( isset($rule[2]) ) {//CHECK FOR ERROR MESSAGE
-                    $this->setRules($rule[0], $rule[1], $rule[2]);
+                if ( isset($rule[RULE_ERROR_MESSAGE]) ) {//CHECK FOR ERROR MESSAGE
+                    $this->setRules($rule[RULE_KEY], $rule[RULE_KEY_NAME], $rule[RULE_METHOD], $rule[RULE_ERROR_MESSAGE]);
                 } else {
-                    $this->setRules($rule[0], $rule[1]);
+                    $this->setRules($rule[RULE_KEY], $rule[RULE_KEY_NAME], $rule[RULE_METHOD]);
                 }
             }
         }
@@ -40,8 +53,8 @@
         public function setValues($key, $value) {
             $this->values[$key] = $value;
         }
-        public function setRules($key, $validation, $error=null) {
-            $this->rules[] = array('key' => $key, 'validation' => $validation, 'error' => $error);
+        public function setRules($key, $name, $method, $error=null) {
+            $this->rules[] = array('key' => $key, 'name' => $name, 'method' => $method, 'error' => $error);
         }
 
         //GETTER
@@ -51,13 +64,17 @@
         public function getRules() {
             return $this->rules;
         }
+		public function getErrors() {
+            return $this->errors;
+        }
 
         //VALIDATE
         public function validate() {
             $result = true;
             foreach ( $this->rules as $k => $rule ) {
-                if ( call_user_func($rule['validation'], $this->values[$rule['key']]) === false ) {
+                if ( call_user_func($rule['method'], $this->values[$rule['key']]) === false ) {
                     $result = false;
+					$this->errors[$rule['key']][] = ( isset($rule['error']) ? sprintf($rule['error'], $rule['name']) : sprintf($this->defaultErrors[$rule['method']], $rule['name']) );
                 }
             }
             return $result;
